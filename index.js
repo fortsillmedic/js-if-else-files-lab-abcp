408805a71f06188b33a459f08886e6d5c174f532
Davis Hilton <fortsillmedic3@gmail.com>
2018-01-13 11:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ var height = "74";
 // Don't worry about this
 module.exports = { name, height
 }
-if (name=="Susan") {
-  console.log ("The nameis correct");
-}
-else {
-  console.log ("Expected the name to be Susan");
-}
-var name = "Susan";
-if( name === "Susan") {
-  conslog.log ("The name is correct");
-}
